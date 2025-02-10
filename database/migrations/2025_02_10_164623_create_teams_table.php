9bb0eab49566de95f5bc90f65ff1b12e6d3a8139
DanieleChecchin <danielechecchin26@gmail.com>
2025-02-10 17:56:24 +0100

    migration for teams and coaches

--- database/migrations/2025_02_10_164623_create_teams_table.php
+++ database/migrations/2025_02_10_164623_create_teams_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,14 @@ return new class extends Migration
     {
         Schema::create('teams', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('stadium');
+            $table->integer('team_value');
+            $table->year('foundation_year');
+            $table->string('president');
+            $table->integer('palmares');
+            $table->string('city');
+            $table->string('main_sponsor');
             $table->timestamps();
         });
     }
--- database/migrations/2025_02_10_164839_create_coaches_table.php
+++ database/migrations/2025_02_10_164839_create_coaches_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,12 @@ return new class extends Migration
     {
         Schema::create('coaches', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('nationality');
+            $table->integer('trophies');
+            $table->date('date_of_birth');
+            $table->text('description');
             $table->timestamps();
         });
     }
