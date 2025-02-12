7b562e4edf0bb1892ad877d977809782110f0030
Beniamino-Soccio <beniamino43@gmail.com>
2025-02-12 17:41:58 +0100

    hotfix template

--- resources/views/admin/sports/layouts/create-edit.blade.php
+++ resources/views/admin/sports/layouts/create-edit.blade.php
@@ -22,37 +22,37 @@
                     @enderror --}}
                 </div>
                 <div class="mb-3">
-                    <label for="player-last_name" class="form-label text-white">player last_name:</label>
+                    <label for="player-last_name" class="form-label">player last_name:</label>
                     <input type="text" name="last_name" id="player-last_name" class="form-control" value="{{ old("last_name", $player->last_name) }}">
 
                 </div>
                 <div class="mb-3">
-                    <label for="player-date_of_birth" class="form-label text-white">player date_of_birth:</label>
+                    <label for="player-date_of_birth" class="form-label">player date_of_birth:</label>
                     <input type="text" name="date_of_birth" id="player-date_of_birth" class="form-control" value="{{ old("date_of_birth", $player->date_of_birth) }}">
 
                 </div>
                 <div class="mb-3">
-                    <label for="player-role" class="form-label text-white">player role:</label>
+                    <label for="player-role" class="form-label">player role:</label>
                     <input type="text" name="role" id="player-role" class="form-control" value="{{ old("role", $player->role) }}">
 
                 </div>
                 <div class="mb-3">
-                    <label for="player-nationality" class="form-label text-white">player nationality:</label>
+                    <label for="player-nationality" class="form-label">player nationality:</label>
                     <input type="text" name="nationality" id="player-nationality" class="form-control" value="{{ old("nationality", $player->nationality) }}">
 
                 </div>
                 <div class="mb-3">
-                    <label for="player-market_value" class="form-label text-white">player market_value:</label>
+                    <label for="player-market_value" class="form-label">player market_value:</label>
                     <input type="text" name="market_value" id="player-market_value" class="form-control" value="{{ old("market_value", $player->market_value) }}">
 
                 </div>
                 <div class="mb-3">
-                    <label for="player-fav_foot" class="form-label text-white">player fav_foot:</label>
+                    <label for="player-fav_foot" class="form-label">player fav_foot:</label>
                     <input type="text" name="fav_foot" id="player-fav_foot" class="form-control" value="{{ old("fav_foot", $player->fav_foot) }}">
 
                 </div>
                 <div class="mb-3">
-                    <label for="player-height" class="form-label text-white">player height:</label>
+                    <label for="player-height" class="form-label">player height:</label>
                     <input type="text" name="height" id="player-height" class="form-control" value="{{ old("height", $player->height) }}">
 
                 </div>
