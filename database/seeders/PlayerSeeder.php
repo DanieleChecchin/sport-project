09463afca60ea0154dfa78c77ec21fb1b55a6577
Beniamino-Soccio <beniamino43@gmail.com>
2025-02-13 13:05:12 +0100

    seeder first player

--- database/seeders/PlayerSeeder.php
+++ database/seeders/PlayerSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Player;
 
 class PlayerSeeder extends Seeder
 {
@@ -12,6 +13,20 @@ class PlayerSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $players = [
+            [
+                "nome" => "Marco",
+                "cognome" => "Carnesecchi",
+                "data_di_nascita" => "2000-07-01",
+                "ruolo" => "Portiere",
+                "nazionalità" => "Italia",
+                "valore_di_mercato" => "€20.00m",
+                "piede_preferito" => "Destro",
+                "altezza" => "1.91m"
+            ],
+        ];
+        foreach ($players as $player) {
+            Player::create($player);
+        }
     }
 }
