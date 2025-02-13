9c6cf84aa559160325867d649aa70be09fb5e938
DanieleChecchin <danielechecchin26@gmail.com>
2025-02-13 15:48:21 +0100

    fixed DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-
+            CoachSeeder::class,
+            TeamSeeder::class,
+            PlayerSeeder::class
         ]);
     }
 }
