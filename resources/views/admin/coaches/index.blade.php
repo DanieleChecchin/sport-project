35668ac4a8271ba42069ac3fee1ce753f1f6446d
DanieleChecchin <danielechecchin26@gmail.com>
2025-02-19 17:06:17 +0100

    hotfix

--- resources/views/admin/coaches/index.blade.php
+++ resources/views/admin/coaches/index.blade.php
@@ -25,7 +25,7 @@
                             <div class="card-body text-center">
                                 <h5 class="card-title fw-bold text-white">{{ $coach->first_name }} {{ $coach->last_name }}
                                 </h5>
-                                <p class="card-text text-white ">🌍 Panchina: <strong>{{ $coach->team }}</strong>
+                                <p class="card-text text-white ">Panchina: <strong>{{ $coach->team }}</strong>
                                 </p>
                                 <a href="{{ route('admin.coaches.show', $coach->id) }}"
                                     class="btn btn-outline-warning rounded-pill px-4 mt-2">📖 Scopri di più</a>
