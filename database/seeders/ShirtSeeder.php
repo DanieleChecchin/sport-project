333f4ede4e5a4956a01ab3c1f446662a6dcc39aa
DanieleChecchin <danielechecchin26@gmail.com>
2025-02-25 17:39:23 +0100

    adde seeder

--- database/seeders/ShirtSeeder.php
+++ database/seeders/ShirtSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Shirt;
 
 class ShirtSeeder extends Seeder
 {
@@ -12,6 +13,32 @@ class ShirtSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $shirts = [
+            ['img' => '1atalanta-retro.png'],
+            ['img' => 'bologna-retro.png'],
+            ['img' => 'cagliari-retro.png'],
+            ['img' => '1comoretro.png'],
+            ['img' => 'empoli-retro.png'],
+            ['img' => '1fiorentinaretro.png'],
+            ['img' => 'genoa-retro.png'],
+            ['img' => 'verona-retro.png'],
+            ['img' => '1interretro.png'],
+            ['img' => '1juveretro.png'],
+            ['img' => 'lazio-retro.png'],
+            ['img' => 'lecce-retro.png'],
+            ['img' => '1milanretro.png'],
+            ['img' => 'monza-retro.png'],
+            ['img' => '1napoliretro.png'],
+            ['img' => 'parma-retro.png'],
+            ['img' => '1romaretro.png'],
+            ['img' => 'torino-retro.png'],
+            ['img' => 'udinese-retro.png'],
+            ['img' => 'venezia-retro.png'],
+
+        ];
+
+        foreach ($shirts as $shirt) {
+            Shirt::create($shirt);
+        }
     }
 }
